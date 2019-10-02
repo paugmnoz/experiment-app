e424c060f13a87972458c662d29fb32126e155a5
Paula <paula.garcia1@correo.icesi.edu.co>
2019-10-01 21:29:17 -0500

    Html structure and gen styles added

--- src/components/SpectroEditorA/SpectroEditorA.tsx
+++ src/components/SpectroEditorA/SpectroEditorA.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
 import './SpectroEditorA.scss';
 import ToolsMenu from '../ToolsMenu/ToolsMenu';
+import { taggingStore } from '../../store/TaggingStore';
+import { observer } from 'mobx-react';
 
+@observer
 export class SpectroEditorA extends React.Component {
-    render(){
-        return <div>  
-                <ToolsMenu/>
-            </div>
+    render() {
+        return <div className="editor-cont">
+            <ToolsMenu />
+            <section className="editor-section">
+                <div className="left">
+                    <span className="spectrogram">
+                        <img src="./assets/Editor/original-gray-spectrum.png" alt="" />
+                    </span>
+                </div>
+                <div className="right">
+                    {
+                        (taggingStore.colorEditionStatus) ? <span className="color-atom">
+                            color
+                        </span> :  (taggingStore.contrastEditionStatus) ?  <span className="contrast-atom">
+                            contrast
+                            </span> : ""
+                    }
+
+
+                </div>
+            </section>
+        </div>
     }
 }

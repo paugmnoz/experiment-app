d153478a717b0933dd47d270465fa78c5e912ef5
Paula <paula.garcia1@correo.icesi.edu.co>
2019-10-01 23:30:14 -0500

    scale selection actions

--- src/store/TaggingStore.ts
+++ src/store/TaggingStore.ts
@@ -81,14 +81,94 @@ class TaggingStore {
 
     //EDITOR A
 
-    @observable colorEditionStatus:boolean = false;
-    @observable contrastEditionStatus:boolean = false;
+    @observable colorEditionStatus: boolean = false;
+    @observable contrastEditionStatus: boolean = false;
+
+
 
     @action onColorEdit() {
         this.colorEditionStatus = true;
         this.contrastEditionStatus = false;
     }
 
+    @observable scaleNameSelected = 'gray';
+
+    @observable scale1Selected = true;
+    @observable scale2Selected = false;
+    @observable scale3Selected = false;
+    @observable scale4Selected = false;
+    @observable scale5Selected = false;
+    @observable scale6Selected = false;
+    @observable scale7Selected = false;
+
+    @action onScaleSelection(scale: string) {
+        switch (scale) {
+            case 'gray':
+                this.scale1Selected = true;
+                this.scale2Selected = false;
+                this.scale3Selected = false;
+                this.scale4Selected = false;
+                this.scale5Selected = false;
+                this.scale6Selected = false;
+                this.scale7Selected = false;
+                break;
+            case '2':
+                this.scale1Selected = false;
+                this.scale2Selected = true;
+                this.scale3Selected = false;
+                this.scale4Selected = false;
+                this.scale5Selected = false;
+                this.scale6Selected = false;
+                this.scale7Selected = false;
+                break;
+            case '3':
+                this.scale1Selected = false;
+                this.scale2Selected = false;
+                this.scale3Selected = true;
+                this.scale4Selected = false;
+                this.scale5Selected = false;
+                this.scale6Selected = false;
+                this.scale7Selected = false;
+                break;
+            case '4':
+                this.scale1Selected = false;
+                this.scale2Selected = false;
+                this.scale3Selected = false;
+                this.scale4Selected = true;
+                this.scale5Selected = false;
+                this.scale6Selected = false;
+                this.scale7Selected = false;
+                break;
+            case '5':
+                this.scale1Selected = false;
+                this.scale2Selected = false;
+                this.scale3Selected = false;
+                this.scale4Selected = false;
+                this.scale5Selected = true;
+                this.scale6Selected = false;
+                this.scale7Selected = false;
+                break;
+            case '6':
+                this.scale1Selected = false;
+                this.scale2Selected = false;
+                this.scale3Selected = false;
+                this.scale4Selected = false;
+                this.scale5Selected = false;
+                this.scale6Selected = true;
+                this.scale7Selected = false;
+                break;
+            case '7':
+                this.scale1Selected = false;
+                this.scale2Selected = false;
+                this.scale3Selected = false;
+                this.scale4Selected = false;
+                this.scale5Selected = false;
+                this.scale6Selected = false;
+                this.scale7Selected = true;
+                break;
+        }
+    }
+    
     @action onContrastEdit() {
         this.contrastEditionStatus = true;
         this.colorEditionStatus = false;

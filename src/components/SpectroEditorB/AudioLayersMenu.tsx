309a828b63fd7cd16d0d5024b390637db1409c77
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-10-01 23:35:17 -0500

    change audiolayersmenu to a component

---
+++ src/components/SpectroEditorB/AudioLayersMenu.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import './SpectroEditorB.scss';
+import Iframe from 'react-iframe'
+import ToolsMenu from '../ToolsMenu/ToolsMenu';
+import '../SpectroEditorB/SpectroEditorB.scss';
+
+export default class AudioLayersMenu extends React.Component {
+
+    render() {
+        return <section className="layers-management">
+        <div className="header-lay-mang">
+            <h2>Capas de Audio</h2>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Todos</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-all-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Micrófono 1</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-1-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Micrófono 2</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-2-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Micrófono 3</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-3-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Micrófono 4</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-4-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Micrófono 5</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-5-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        <div className="mic-container">
+            <span>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/unlocked-padlock.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/sound-icon.svg" alt="" height="30" width="30"></img></button>
+            <button className="mic-cont-btn"><img src="./assets/layers-manag-assets/eye-icon.svg" alt="" height="30" width="30"></img></button>
+            </span>
+            <h3>Micrófono 6</h3>
+            <span className="hex">
+            <img src="./assets/layers-manag-assets/hex-6-icon.svg" alt="" height="30" width="30"></img>
+            <h3>HEX</h3>
+            </span>
+        </div>
+        </section>;
+    }
+}
+

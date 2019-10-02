887728014188893fca46b7757e54f75d8b3c9170
Paula <paula.garcia1@correo.icesi.edu.co>
2019-10-01 20:19:37 -0500

    iframe deleted and tool menu added

--- src/components/SpectroEditorA/SpectroEditorA.tsx
+++ src/components/SpectroEditorA/SpectroEditorA.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import './SpectroEditorA.scss';
-import Iframe from 'react-iframe'
+import ToolsMenu from '../ToolsMenu/ToolsMenu';
 
 export class SpectroEditorA extends React.Component {
     render(){
         return <div>  
-        <Iframe url="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FmHHdlqPuwYLGhb1ADJEIna%2FSprint-4%3Fnode-id%3D43%253A1348%26scaling%3Dscale-down-width"
-        width="1200px"
-        height="615px"
-        id="embed"
-        className="myClassname"
-        position="relative"/>
-        </div>
+                <ToolsMenu/>
+            </div>
     }
 }

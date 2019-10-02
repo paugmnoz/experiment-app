2322557f41291950240a14be39fac7c6fed90a09
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-10-01 19:58:01 -0500

    gitflow-feature-stash: seb
    tool menu done

--- src/components/SpectroEditorB/SpectroEditorB.tsx
+++ src/components/SpectroEditorB/SpectroEditorB.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import './SpectroEditorB.scss';
 import Iframe from 'react-iframe'
+import ToolsMenu from '../ToolsMenu/ToolsMenu';
 
 export class SpectroEditorB extends React.Component {
 
     render(){
         return <div>  
-        <Iframe url="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FmHHdlqPuwYLGhb1ADJEIna%2FSprint-4%3Fnode-id%3D44%253A1042%26scaling%3Dscale-down-width"
-        width="1200px"
-        height="615px"
-        id="embed"
-        className="myClassname"
-        position="relative"/>
+        <ToolsMenu/>
+        <section className="layers-management">
+            <h2>Capas de Audio</h2>
+        </section>
         </div>
     }
 }
---
+++ src/components/ToolsMenu/ToolsMenu.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import '../ToolsMenu/ToolsMenu.scss'
+
+export default class ToolsMenu extends React.Component {
+
+    render() {
+        return <div className="tool-bar">
+            <div className="audio-options">
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/play-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Reproducir</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/pause-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Pausar</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/advance-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Avanzar</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/stop-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Parar</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/volume-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Volumen</span>
+                </span>
+            </div>
+            <div className="edition-options">
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/arrow-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Cursor</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/select-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Realizar una selección</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/zoom-in-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Acercar</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/zoom-out-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Alejar</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/color-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Mapa de Color</span>
+                </span>
+                <span className="tooltip">
+                    <button className="tool-btn"><img src="./assets/tool-bar-assets/bright-icon.svg" alt="" height="50" width="50"></img></button>
+                    <span className="tooltiptext">Brillo/Contraste</span>
+                </span>
+            </div>
+        </div>
+    }
+}

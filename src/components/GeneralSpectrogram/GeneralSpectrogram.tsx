c38e6e901ae0bc10fbffd5ec732e4abf9351dba3
Paula <paula.garcia1@correo.icesi.edu.co>
2019-10-06 18:56:19 -0500

    general spectrogram component

---
+++ src/components/GeneralSpectrogram/GeneralSpectrogram.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './GeneralSpectrogram.scss';
+import Iframe from 'react-iframe'
+import { taggingStore } from '../../store/TaggingStore';
+import { observer } from 'mobx-react';
+import { SpectrogramCompareView } from '../subcomponents/SpectrogramCompareView/SpectrogramCompareView';
+
+@observer
+export class GeneralSpectrogram extends React.Component {
+
+    render() {
+        return <div className="gen-spectro-section">
+
+        </div>
+    }
+}

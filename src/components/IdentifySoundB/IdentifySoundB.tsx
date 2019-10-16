54f7312002927de2c02493932cfd865494793162
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-10-16 00:36:10 -0500

    css of identify sound b

--- src/components/IdentifySoundB/IdentifySoundB.tsx
+++ src/components/IdentifySoundB/IdentifySoundB.tsx
@@ -18,7 +18,7 @@ import { relative, isAbsolute } from 'path';
 
     constructor(props) {
         super(props);
-      }
+    }
 
     render() {
         return <section >
@@ -27,54 +27,46 @@ import { relative, isAbsolute } from 'path';
                 <img className="close-icon" src="./assets/icons/close.svg" alt="" />
             </div>
             <div className="card-section">
-                <img className="spectro"  
-                src="./assets/espectogramaB.png" alt="" onClick={() => {
-                    this.showTagOption = !this.showTagOption;
-                    this.posXCard = taggingStore.posXCard;
-                    this.posYCard = taggingStore.posYCard;
-                    console.log('printing');
-                }} />
+                <img className="spectro"
+                    src="./assets/espectogramaB.png" alt="" width="600" />
             </div>
             <div className="card-section" id="card-for-tagging">
-                    <span className="description-items-row">
-                        <p><span className="text-title">Fecha:</span> 28 Julio 2019</p>
-                        <p><span className="text-title">Hora:</span> 06:36 AM</p>
-                        <p><span className="text-title">Duración:</span> 5s</p>
-                    </span>
-                    <div className="card-item" style={{
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}>
-                        <p>Especies estudiando</p>
-                        <img src="./assets/Birds.png" 
-                        onClick={() => { 
+                <span className="description-items-row">
+                    <p><span className="text-title">Fecha:</span> 28 Julio 2019</p>
+                    <p><span className="text-title">Hora:</span> 06:36 AM</p>
+                    <p><span className="text-title">Duración:</span> 5s</p>
+                </span>
+                <div className="card-item" style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+                    <p>Especies estudiando</p>
+                    <img src="./assets/Birds.png"
+                        onClick={() => {
                             taggingStore.birdClick()
-                        } }
+                        }}
                         alt="" className="img-birds" width="246"></img>
-                    </div>
-                    <span className="input-row card-item">
-                        <span className="input-col "> <p className="text-title"> Nombre Común:</p> <input type="text" value={taggingStore.commonName} /></span>
-                        <span className="input-col "> <p className="text-title"> Nombre Científico:</p> <input type="text" value={taggingStore.sciName} /></span>
-                    </span>
-                    <span className="input-row card-item">
-                        <p className="text-title">Tipo de canto:</p>
-                        <select>
+                </div>
+                <span className="input-row card-item">
+                    <span className="input-col "> <p className="text-title"> Nombre Común</p> <input type="text" value={taggingStore.commonName} /></span>
+                    <span className="input-col "> <p className="text-title"> Nombre Científico</p> <input type="text" value={taggingStore.sciName} /></span>
+                    <span className="input-col "><p className="text-title">Tipo de canto</p>
+                        <select className="song-type">
                             <option>Canto</option>
                             <option>Llamador</option>
                             <option>Cortejo</option>
-                        </select>
-                    </span>
-                    <span className="input-col card-item"> <p className="text-title"> Anotación/Comentario:</p> <input type="text" /></span>
-                    <p className="card-item">Marcar como:</p>
+                        </select></span>
+                </span>
+                <span className="input-col card-item"> <p className="text-title"> Anotación/Comentario:</p> <textarea style={{ height: 50 }}> </textarea></span>
+                <p className="card-item">Marcar como:</p>
+                <div className="save-options">
                     <label className="card-item"><input type="checkbox" id="myCheck"></input>Seguro</label>
                     <label className="card-item"><input type="checkbox" id="myCheck"></input>Pendiente</label>
                     <label className="card-item"><input type="checkbox" id="myCheck"></input>Duda</label>
-                    <div className="btns">
-                        <button className="green-button" style={{}}>Guardar</button>
-                    </div>
+                    <button className="green-button" style={{}}>Guardar</button>
                 </div>
-                : ''}
-           <div className="card-section">
+            </div>
+            <div className="card-section">
                 <p className="hint-text">¿Alguna duda sobre el canto? Verifica con cantos de Xeno-Canto</p>
                 <span className="input-row search card-item">
                     <p className="text-title">Buscar en Xeno-Canto:</p> <input type="text" />

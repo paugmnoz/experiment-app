00e67df01313eff832e35dac7b33ebe6bd3e6538
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-10-16 12:03:25 -0500

    2B 99% done

--- src/store/TaggingStore.ts
+++ src/store/TaggingStore.ts
@@ -83,12 +83,18 @@ class TaggingStore {
                 this.comparedMicro6Activated = !this.comparedMicro6Activated
                 break;
             case 'all':
-                this.comparedMicro1Activated = !this.comparedMicro1Activated
+                //this.comparedMicro1Activated = !this.comparedMicro1Activated
                 this.comparedMicro2Activated = !this.comparedMicro2Activated
                 this.comparedMicro3Activated = !this.comparedMicro3Activated
                 this.comparedMicro4Activated = !this.comparedMicro4Activated
                 this.comparedMicro5Activated = !this.comparedMicro5Activated
                 this.comparedMicro6Activated = !this.comparedMicro6Activated
+                console.log('mic 1', this.comparedMicro1Activated);
+                console.log('mic 2', this.comparedMicro2Activated);
+                console.log('mic 3', this.comparedMicro3Activated);
+                console.log('mic 4', this.comparedMicro4Activated);
+                console.log('mic 5', this.comparedMicro5Activated);
+                console.log('mic 6', this.comparedMicro6Activated);
                 break;
         }
     }
